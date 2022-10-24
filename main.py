f13ef222c3edcddb483b9312e8d1cfdc7d8e5a49
Safi <safi.sharieff@gmail.com>
2022-10-24 19:59:23 +0400

    Data has been prepared

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
 import pandas as pd
+import matplotlib as plt
+import nltk
+import numpy
+import string
+
+
+def main():
+    data_sample = pd.read_csv('Tweets.csv')
+    #print(data_sample.columns)
+    #print(data_sample['text'])
+    #print(data_sample['airline_sentiment'])
+
+    relevant_sample = data_sample[ ['text', 'airline_sentiment'] ]
+    print(relevant_sample.head())
+
+
+if __name__ == '__main__':
+    main()
