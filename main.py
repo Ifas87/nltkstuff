48ad0b20bd2f314f9b47ee2039acf3db2c61ea57
Safi <safi.sharieff@gmail.com>
2022-10-26 17:51:42 +0400

    Added Tokenizers and parsing the text series through it

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ import matplotlib as plt
 import nltk
 import numpy
 import string
-
+import tensorflow
+import tensorflow_text as tf
 
 def main():
     data_sample = pd.read_csv('Tweets.csv')
@@ -13,9 +14,14 @@ def main():
 
     relevant_sample = data_sample[ ['text', 'airline_sentiment'] ]
     #print(relevant_sample.head())
-    #print(relevant_sample["airline_sentiment"].value_counts())
-
-
+    # print(relevant_sample["airline_sentiment"].value_counts())
+    # print(relevant_sample.shape)
+    relevant_sample = relevant_sample[relevant_sample['airline_sentiment'] != "neutral"]
+    # print(relevant_sample["airline_sentiment"].value_counts())
+    # print(relevant_sample["airline_sentiment"].factorize())
+    nptext = relevant_sample['text'].values
+    tokenisers = tensorflow.keras.preprocessing.text.Tokenizer(num_words=5000)
+    tokenisers.fit_on_texts(nptext)
 
 
 if __name__ == '__main__':
