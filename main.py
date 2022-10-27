edd136c204398ab82a7f93d365e23e92c332780b
Safi <safi.sharieff@gmail.com>
2022-10-27 19:12:31 +0400

    Decided and Imported the required ML model

--- main.py
+++ main.py
@@ -6,6 +6,10 @@ import string
 import tensorflow
 import tensorflow_text as tf
 
+from tensorflow.python.keras.models import Sequential
+from tensorflow.python.keras.layers import LSTM,Dense, Dropout, SpatialDropout1D
+from tensorflow.python.keras.layers import Embedding
+
 def main():
     data_sample = pd.read_csv('Tweets.csv')
     #print(data_sample.columns)
@@ -22,6 +26,10 @@ def main():
     nptext = relevant_sample['text'].values
     tokenisers = tensorflow.keras.preprocessing.text.Tokenizer(num_words=5000)
     tokenisers.fit_on_texts(nptext)
+
+    encoded_result = tokenisers.texts_to_sequences(nptext)
+    padded_result = tensorflow.keras.preprocessing.sequence.pad_sequences(encoded_result, maxlen=200)
+
 
 
 if __name__ == '__main__':
