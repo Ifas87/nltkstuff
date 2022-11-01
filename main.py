f964f8f53d5c0ca96433e7f3d69e8cf481a45aff
Safi <safi.sharieff@gmail.com>
2022-11-01 17:41:08 +0400

    Made minor fixes to a Graph printing

--- main.py
+++ main.py
@@ -8,6 +8,13 @@ from tensorflow.python.keras.layers import LSTM,Dense, Dropout, SpatialDropout1D
 from tensorflow.python.keras.layers import Embedding
 
 
+def sentiment_analysis(text, tokenisers, model, sentiment_label):
+    tw = tokenisers.texts_to_sequences([text])
+    tw = tensorflow.keras.preprocessing.sequence.pad_sequences(tw,maxlen=200)
+    prediction = int(model.predict(tw).round().item())
+    print("Predicted label: ", sentiment_label[1][prediction])
+
+
 def main():
     data_sample = pd.read_csv('Tweets.csv')
     #print(data_sample.columns)
@@ -43,21 +50,20 @@ def main():
     history = model.fit(padded_result, sentiment_label[0], validation_split=0.2, epochs=5, batch_size=32)
 
     
-    plt.plot(history.history['accuracy'], label='acc')
-    plt.plot(history.history['val_accuracy'], label='val_acc')
-    plt.legend()
-    plt.show()
+    # plt.plot(history.history['accuracy'], label='acc')
+    # plt.plot(history.history['val_accuracy'], label='val_acc')
+    # plt.legend()
+    # plt.savefig("Accuracy plot.jpg")
+    # plt.show()
 
-    plt.savefig("Accuracy plot.jpg")
-
-    plt.plot(history.history['loss'], label='loss')
-    plt.plot(history.history['val_loss'], label='val_loss')
-    plt.legend()
-    plt.show()
+    # plt.plot(history.history['loss'], label='loss')
+    # plt.plot(history.history['val_loss'], label='val_loss')
+    # plt.legend()
+    # plt.savefig("Loss plt.jpg")
+    # plt.show()
     
-    plt.savefig("Loss plt.jpg")
-
-
+    example_texts_1 = "I had a good and bad time there."
+    sentiment_analysis(example_texts_1, tokenisers, model, sentiment_label)
 
 
 if __name__ == '__main__':
