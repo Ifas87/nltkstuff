56908d1324a8e1e18017528812645f6d87543841
Safi <safi.sharieff@gmail.com>
2022-11-02 10:37:28 +0400

    Generated summary statistics on the sample data.

--- emotions.py
+++ emotions.py
@@ -5,8 +5,10 @@ import pandas as pd
 def main():
     sample_data = pd.read_csv('Tweets.csv')["text"]
     bigString = ",".join(sample_data)
-    # print(bigString)
+    
     text_stuff = NRCLex(bigString)
+    results = text_stuff.raw_emotion_scores
+    print(results)
 
 
 if __name__ == '__main__':
